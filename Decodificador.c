baa3b3c28b38e1c6fc892b6c63c5fb86c476bb25
Uarlley do Nascimento Amorim <51249018+iHateAnimes@users.noreply.github.com>
2020-10-05 20:12:36 -0300

    Update Decodificador.c

--- Decodificador.c
+++ Decodificador.c
@@ -1,4 +1,12 @@
-#include "cod.h"
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+typedef struct tnode{
+    struct tnode* prev;
+    struct tnode* next;
+    char chr;
+}node;
 
 node* createNode(char chr){
     node* newnode = NULL;
